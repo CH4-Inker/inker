203b334c757bcf1158398460ffa7620f31541ade
YohanesVitoMBP <yohanes.vito@ti.ukdw.ac.id>
2026-07-10 02:53:27 +0800

    Rename iOS companion target to inker and add app icon assets
    Xcode-driven rename of the companion target from companionapp to
    inker, with new asset catalog, app icon, and Inker.icon set.

---
+++ inker/inkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  inkerApp.swift
+//  inker
+//
+//  Created by Yohanes Vito Rizki D on 09/07/26.
+//
+
+import SwiftUI
+
+@main
+struct inkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
